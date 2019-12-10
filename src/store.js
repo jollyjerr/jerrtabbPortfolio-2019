e13d38fcdf33a69da9354374738cf28b6367a231
jollyjerr <jollyjerr@gmail.com>
2019-12-09 21:12:02 -0700

    :ambulance: fix broken img

--- src/store.js
+++ src/store.js
@@ -1,38 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import harmonicPhoto from '../src/assets/save.gif';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    projects: [{
-      id: 1,
-      name: 'footprint',
-      description: 'A personalized chatbot to help users make sustainable decisions and reduce their carbon footprint.',
-      link: 'https://github.com/jollyjerr/footprints-frontend',
-      image: 'https://github.com/jollyjerr/footprints-backend/raw/master/github/chat.gif'
-    }, {
-      id: 2,
-      name: 'Stellar Navigation',
-      description: 'An interactive portrayal of stellar systems that calculates travel time between celestial bodies.',
-      link: 'https://github.com/jollyjerr/StellarNavigation-Frontend',
-      image: 'https://github.com/jollyjerr/StellarNavigation-Backend/raw/master/GitHubFiles/intro.gif'
-    }, {
-      id: 3,
-      name: 'Harmonics',
-      description: 'A web application that recommends chord progressions according to principles of tonal harmony as a user writes music.',
-      link: 'https://github.com/jollyjerr/Harmonics',
-      image: 'https://github.com/jollyjerr/Harmonics/blob/harmonics2.0/github/save.gif'
-    }, {
-      id: 4,
-      name: 'Trip-Up',
-        description: 'A web application for scheduling trips with your friends.',
-      link: 'https://github.com/jollyjerr/Trip-Up',
-      image: 'https://github.com/jollyjerr/Trip-Up/raw/master/github/comments.gif'
-    }]
+    projects: [
+      {
+        id: 1,
+        name: "footprint",
+        description:
+          "A personalized chatbot to help users make sustainable decisions and reduce their carbon footprint.",
+        link: "https://github.com/jollyjerr/footprints-frontend",
+        image:
+          "https://github.com/jollyjerr/footprints-backend/raw/master/github/chat.gif"
+      },
+      {
+        id: 2,
+        name: "Stellar Navigation",
+        description:
+          "An interactive portrayal of stellar systems that calculates travel time between celestial bodies.",
+        link: "https://github.com/jollyjerr/StellarNavigation-Frontend",
+        image:
+          "https://github.com/jollyjerr/StellarNavigation-Backend/raw/master/GitHubFiles/intro.gif"
+      },
+      {
+        id: 3,
+        name: "Harmonics",
+        description:
+          "A web application that recommends chord progressions according to principles of tonal harmony as a user writes music.",
+        link: "https://github.com/jollyjerr/Harmonics",
+        image: harmonicPhoto
+      },
+      {
+        id: 4,
+        name: "Trip-Up",
+        description:
+          "A web application for scheduling trips with your friends.",
+        link: "https://github.com/jollyjerr/Trip-Up",
+        image:
+          "https://github.com/jollyjerr/Trip-Up/raw/master/github/comments.gif"
+      }
+    ]
   },
-  mutations: {
-  },
-  actions: {
-  }
-})
+  mutations: {},
+  actions: {}
+});
